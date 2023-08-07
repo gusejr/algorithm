cb6cfdb8d87c6cebd45160883690dd45f2a51b94
gusejr <87484422+gusejr@users.noreply.github.com>
2023-08-07 15:44:16 +0900

    [Silver V] Title: 다리 놓기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/1010. 다리 놓기/다리 놓기.cc
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <utility>
+#include <climits>
+#include <cmath>
+
+using namespace std;
+int dp[31][31] = { 0 };
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+    int T;
+    int west_point, east_point;
+    cin >> T;
+    for (int i = 0; i < T; i++)
+    {
+        dp[31][31] = { 0 };
+        cin >> west_point >> east_point;
+        for (int j = 1; j <= east_point; j++)
+        {
+            dp[1][j] = dp[1][j - 1] + 1;
+        }
+        for (int j = 2; j <= west_point; j++)
+        {
+            for (int k = 1; k <= east_point; k++)
+            {
+                dp[j][k] = dp[j][k - 1] + dp[j - 1][k - 1];
+            }
+        }
+        cout << dp[west_point][east_point] << '\n';
+    }
+}
