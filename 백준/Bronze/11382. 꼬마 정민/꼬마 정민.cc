b0e72a88329c2c61d122d78cb31fd38c47da104a
gusejr <87484422+gusejr@users.noreply.github.com>
2023-11-06 15:05:12 +0900

    [Bronze V] Title: 꼬마 정민, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/11382. 꼬마 정민/꼬마 정민.cc
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <queue>
+#include <utility>
+#include <climits>
+#include <cmath>
+
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+    long long a, b, c;
+    cin >> a >> b >> c;
+    cout << a + b + c << '\n';
+}
