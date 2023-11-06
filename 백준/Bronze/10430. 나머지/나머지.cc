5ce2d4c2d2baf3b716432169b9962a1981bcb4ba
gusejr <87484422+gusejr@users.noreply.github.com>
2023-11-06 14:57:53 +0900

    [Bronze V] Title: 나머지, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10430. 나머지/나머지.cc
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <queue>
+#include <utility>
+#include <climits>
+#include <cmath>
+
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+    int a, b, c;
+    cin >> a >> b >> c;
+
+    cout << (a + b) % c << '\n';
+    cout << ((a % c) + (b % c)) % c << '\n';
+    cout << (a * b) % c << '\n';
+    cout << ((a % c) * (b % c)) % c << '\n';
+}
