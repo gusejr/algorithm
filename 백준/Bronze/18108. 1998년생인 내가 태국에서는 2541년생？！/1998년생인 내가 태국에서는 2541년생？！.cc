4df57f4bd29de54cba4ec24fb2dc4cd4fbd7e349
gusejr <87484422+gusejr@users.noreply.github.com>
2023-11-06 14:49:36 +0900

    [Bronze V] Title: 1998년생인 내가 태국에서는 2541년생?!, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/18108. 1998년생인 내가 태국에서는 2541년생？！/1998년생인 내가 태국에서는 2541년생？！.cc
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <queue>
+#include <utility>
+#include <climits>
+#include <cmath>
+
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+    int Buddhism_year;
+    cin >> Buddhism_year;
+    cout << Buddhism_year - 543 << '\n';
+}
