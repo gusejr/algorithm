cb4a61d707d1f13abc787114e3c6e09bdec91984
gusejr <87484422+gusejr@users.noreply.github.com>
2023-11-14 15:52:39 +0900

    [Bronze V] Title: A+B - 8, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/11022. A＋B － 8/A＋B － 8.cc
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <queue>
+#include <utility>
+#include <climits>
+#include <cmath>
+
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+    int T;
+    int A, B;
+    cin >> T;
+    for (int i = 0; i < T; i++)
+    {
+        cin >> A >> B;
+        cout << "Case #" << i + 1 << ": " << A << " + " << B << " = " << A + B << '\n';
+    }
+}
